3afd8adced613e95f723b81e6f35084a9855e847
Diegho <dieghonm@gmail.com>
2022-11-23 00:13:48 -0300

    Adciona informaçoes na tela ComoJogar

--- src/pages/ComoJogar.jsx
+++ src/pages/ComoJogar.jsx
@@ -5,7 +5,19 @@ class ComoJogar extends Component {
     return(
       <div>
         <h3>ComoJogar</h3>
-        <h6></h6>
+        <p>
+          Aventuras fantasticas e umaclassico jogo de RPG, nele vamos ter pontos de vida e habilidades
+          Algumas vezes teremos magia, outras sorte, cada edição e diferente da anterior.
+          Mas todos sempre teram a mesma dinamida de combates, e testes de habilidade.
+        </p>
+        <ul>
+          <li>Atributos</li>
+          <li>Vida</li>
+          <li>Equipamento</li>
+          <li>Tesouros</li>
+          <li>Inimigos</li>
+          <li>Combate</li>
+        </ul>
       </div>
     )
   }
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -10,13 +10,18 @@ class Project extends Component {
         {/* <S.fundoImg src={fundo} alt="" /> */}
         <S.projectText>
           Na década de 1990 foi publicado pela editora MARQUES SARAIVA a coleção de livos jogos aventuras fantásticas,
-          estes livros continham pequenas historias interativas no estilo RPG, onde o leitor podia escolher como a história iria prosseguir
-          tendo a possibilidade de enfrentar monstros e desvendar enigmas, tudo com a ajuda de um lápis e simples dado.
+          estes livros continham pequenas historias interativas no estilo RPG, onde o leitor podia escolher como a história 
+          iria prosseguir tendo a possibilidade de enfrentar monstros e desvendar enigmas, 
+          tudo com a ajuda de um lápis e simples dado.
+          <br />  <br />
+          Aventuras fantasticas e um jogo de RPG, onde cada livro e um labirinto construido em um mundo fantastico onde
+          se pode encontrar criaturas mitologicas, magos, gladiadores ou ate cientistas malucos.
+          cada livro era unico e sua historia totalmente diferente das outras.
           <br />  <br />
           Este projeto e uma tentativa de trazer estes livros de uma forma dinâmica para a nossa atualidade.
           <br /> <br />
           Juntamos aqui um programador e um designer para desenvolver este app, de forma a praticar nossos 
-          conhecimentos e enriquecer nossos portfólios
+          conhecimentos e enriquecer nossos portfólios.
 
         </S.projectText>
       </S.mainDiv>
