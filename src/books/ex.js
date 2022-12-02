9b0748e08c2d0ed8a506281ed4de16b579707e9b
Diegho <dieghonm@gmail.com>
2022-12-02 01:28:35 -0300

    Prepara dados do livro cidadela para ser consumido pela aplicação 0.3

--- src/books/ex.js
+++ src/books/ex.js
@@ -2,22 +2,80 @@ const ex = {
   item:[
     mirfade,
     aranha,
-    amoras
+    amoras,
+    ouro
   ],
+  itemAdd:{
+    'adaga encantada':`adaga de arremesso encantada que nunca erra, p15, 2 pontos de dano, 1vez`
+  },
+
   encanto:[
     copia,
     forca,
     fraqueza,
-    Percepção,
+    percepção,
     ouro,
+    fogo,
+    ilusao,
+    escudo,
   ],
   combate:{
     'ELFO NEGRO':{
       energia: 4,
       habilidade: 4
-    }
+    },
+    'GARK':{
+      energia: 11,
+      habilidade: 7
+    },
+
   },
-  energia:'cura x pontos',
-  nda:'vai direto para outra pagina'
+  'fugir': 'quantidade de ataques',
+  'energia':'cura ou perde x pontos',
+  'nda':'vai direto para outra pagina',
+  'sorte':[sim, nao], //410
+  'dado':[1,2,3,4,5,6],
+  't-soete': 'teste de sorte', // ver o q fazer com o anel 381
+
+  'extrapages':{
+    410: 'teste de sorte 390',
+    420: 'rolada de dado 384' ,
+    450: 'Faca­ Faquinha',
+    500:'fim do jogo 400',
+    600: 'morte 376, 369',
+
+  },
+  'jogo':{
+    'Faca­ Faquinha': 'sorteie uma de 6 facas, se for a sorteada voce morre, 365'
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
